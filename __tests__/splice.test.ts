b660db0edfd0eda9859f5e9a690fe920f8f020ae
Kimmo Kiiski <kimmo@kii.ski>
2020-01-26 22:09:16 +0200

    Added `splice` function

---
+++ __tests__/splice.test.ts
@@ -0,0 +1,30 @@
+import splice from '../splice';
+
+describe('splice()', () => {
+  it('deletes items from the array', () => {
+    expect(splice([1, 2, 3, 4, 5], 1, 2)).toEqual([1, 4, 5]);
+  });
+  it('inserts items into the array', () => {
+    expect(splice([1, 2, 5], 2, 0, 3, 4)).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('deletes and inserts from/into the array', () => {
+    expect(splice([1, 1, 5], 1, 1, 2, 3, 4)).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('returns a copy of the array when changed', () => {
+    const array = [1, 2, 3];
+    expect(splice([1, 2, 3, 4, 5], 1, 2)).not.toBe(array);
+  });
+  it('does not mutate the original array', () => {
+    const array = [1, 2, 3];
+    splice(array, 1, 1, 4, 5);
+    expect(array).toEqual([1, 2, 3]);
+  });
+  it('returns the original array instance if no changes were applied', () => {
+    const array = [1, 2, 3];
+    expect(splice(array, 1, 0)).toBe(array);
+  });
+  it('returns the original array if empty', () => {
+    const array: number[] = [];
+    expect(splice(array, 1, 1)).toBe(array);
+  });
+});
---
+++ splice.ts
@@ -0,0 +1,20 @@
+/**
+ * Changes an array by removing or replacing existing elements and/or adding new elements.
+ * Unlike JavaScript's native `splice` method, this returns a new copy and does not mutate
+ * the original array. If the splice operation would result in no changes, then the reference
+ * to the original array is removed.
+ * @param array array to splice
+ * @param fn function used to transform each item
+ */
+function splice<T>(array: T[], start: number, deleteCount: number): T[];
+function splice<T>(array: T[], start: number, deleteCount: number, ...items: T[]): T[];
+function splice<T>(array: T[], start: number, deleteCount: number, ...items: T[]): T[] {
+  if ((!deleteCount || !array.length) && !items.length) {
+    return array;
+  }
+  const result = array.slice();
+  result.splice(start, deleteCount, ...items);
+  return result;
+}
+
+export default splice;
