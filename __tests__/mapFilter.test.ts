dac942aa2d0d15598235775442875fdc99f6d317
Kimmo Kiiski <kimmo@kii.ski>
2020-01-26 21:32:45 +0200

    Added `mapFilter` function

---
+++ __tests__/mapFilter.test.ts
@@ -0,0 +1,30 @@
+import mapFilter from '../mapFilter';
+
+describe('mapFilter', () => {
+  it('transforms each value in the array', () => {
+    expect(mapFilter([1, 2, 3], (value) => value * 2)).toEqual([2, 4, 6]);
+  });
+  it('filters out values that transform to undefined', () => {
+    expect(mapFilter([1, 2, 3, 4, 5], (v) => (v % 2 ? v * 2 : undefined))).toEqual([2, 6, 10]);
+  });
+  it('does not filter out values null transform values', () => {
+    expect(mapFilter([1, 2, 3], (v) => (v % 2 ? v * 2 : null))).toEqual([2, null, 6]);
+  });
+  it('returns another array instance with transformed values', () => {
+    const array = [1, 2, 3];
+    expect(mapFilter(array, (value) => value * 2)).not.toBe(array);
+  });
+  it('does not mutate the original array', () => {
+    const array = [1, 2, 3];
+    mapFilter(array, (value) => value * 2);
+    expect(array).toEqual([1, 2, 3]);
+  });
+  it('returns the original array instance if no changes were applied', () => {
+    const array = [1, 2, 3];
+    expect(mapFilter(array, (value) => value * 1)).toBe(array);
+  });
+  it('returns the original array if empty', () => {
+    const array: number[] = [];
+    expect(mapFilter(array, (value) => value * 2)).toBe(array);
+  });
+});
---
+++ mapFilter.ts
@@ -0,0 +1,29 @@
+/**
+ * Transforms every value in the array using the given function and returns an array
+ * with the transformed values. If the function does not return a value, or returns
+ * undefined, then the value will be omitted from the final array.
+ *
+ * If every transformed value are the same than the original values and no values
+ * were filtered out, or if the original array is empty, the original array instance
+ * is returned instead of a new reference.
+ * @param array array of values to transform or filter
+ * @param fn function to determine the transformed value, or undefined
+ */
+function mapFilter<T, U>(array: T[], fn: (value: T, index: number) => U | undefined | void): U[] {
+  let altered = false;
+  const result: U[] = [];
+  array.forEach((value, index) => {
+    const transformed = fn(value, index);
+    if (typeof transformed === 'undefined') {
+      altered = true;
+    } else {
+      result.push(transformed);
+      if (!Object.is(transformed, value)) {
+        altered = true;
+      }
+    }
+  });
+  return altered ? result : (array as unknown[] as U[]);
+}
+
+export default mapFilter;
